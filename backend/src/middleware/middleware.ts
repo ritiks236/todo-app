42b9afc7e217d4f7ab9d6b030ce4b3c185803d0f
ritiks236 <ritiks236@gmail.com>
2024-02-15 16:43:38 +0530

    Updated Middlewares

--- backend/src/middleware/middleware.ts
+++ backend/src/middleware/middleware.ts
@@ -2,22 +2,26 @@ import { Request, Response, NextFunction } from "express";
 import { JWT_SECRET } from "../config";
 import jwt from "jsonwebtoken";
 
-export const authMiddleware = async(req :Request, res :Response, next :NextFunction) => {
+interface CustomRequest extends Request {
+    userId? :string
+}
+
+export const authMiddleware = (req :CustomRequest, res :Response, next :NextFunction) => {
     const authHeader = req.headers.authorization;
     
     if(!authHeader || !authHeader.startsWith('Bearer ')){
-        return res.status(404).json("Token Not found")
+        return res.status(404).json("Unauthorized - Token Not found")
     }
 
     const token = authHeader.split(' ')[1];
 
     try{
 
-        const decoded = jwt.verify(token, JWT_SECRET);
-        console.log(decoded)
+        const decoded =  jwt.verify(token, JWT_SECRET) as {userId :string};
+        req.userId = decoded.userId
 
         next();
     } catch(err){
-        return res.status(411).json("Error in token");
+        return res.status(411).json("Internal Server Error - Error in token");
     }
 }
