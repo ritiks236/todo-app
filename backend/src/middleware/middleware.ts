d2cf71bbb99d460f109fd96d80223cb8755ee063
ritiks236 <ritiks236@gmail.com>
2024-02-15 15:55:10 +0530

    MiddleWare Completed

--- backend/src/middleware/middleware.ts
+++ backend/src/middleware/middleware.ts
@@ -1,1 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "../config";
+import jwt from "jsonwebtoken";
 
+export const authMiddleware = async(req :Request, res :Response, next :NextFunction) => {
+    const authHeader = req.headers.authorization;
+    
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(404).json("Token Not found")
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try{
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        console.log(decoded)
+
+        next();
+    } catch(err){
+        return res.status(411).json("Error in token");
+    }
+}
--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -1,10 +1,15 @@
-import express, { Router } from "express";
+import express, { Router, Request, Response } from "express";
 const todoRouter: Router = express.Router();
 import zod from 'zod';
+import { authMiddleware } from "../middleware/middleware";
 
 const todoBody = zod.object({
     title: zod.string(),
     description: zod.string()
+})
+
+todoRouter.post('/createtodo', authMiddleware, async(req :Request, res :Response) => {
+
 })
 
 export default todoRouter;
