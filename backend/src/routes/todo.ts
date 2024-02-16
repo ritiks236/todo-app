ad140d5fa5462efd37aee5ca56ce23c3d36721e4
ritiks236 <ritiks236@gmail.com>
2024-02-16 11:34:38 +0530

    Backend Updated

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -1,77 +1,77 @@
 import express, { Router, Request, Response } from "express";
 const todoRouter: Router = express.Router();
-import zod, { number } from 'zod';
+import zod from 'zod';
 import { authMiddleware } from "../middleware/middleware";
 import { createTodo, deleteTodo, getTodo, markDoneTodo } from "../db/todo";
 
 const todoBody = zod.object({
-    title: zod.string(),
-    description: zod.string()
+  title: zod.string(),
+  description: zod.string()
 })
 
 interface CustomRequest extends Request {
-    userId ?:string
+  userId?: string
 }
 
 
-todoRouter.post('/createtodo', authMiddleware , async(req :CustomRequest, res :Response) => {
-    const {success} = todoBody.safeParse(req.body);
+todoRouter.post('/createtodo', authMiddleware, async (req: CustomRequest, res: Response) => {
+  const { success } = todoBody.safeParse(req.body);
 
-    if(!success) {
-        return res.status(411).json("Incorrect Inputs");
-    }
+  if (!success) {
+    return res.status(411).json("Incorrect Inputs");
+  }
 
-    const {title, description} = req.body;
-    const userId :number = Number(req.userId)
+  const { title, description } = req.body;
+  const userId: number = Number(req.userId)
 
-    const result = await createTodo(userId, {title, description});
+  const result = await createTodo(userId, { title, description });
 
-    if(result){
-        return res.status(200).json({
-            message : "Todo Created Successfully"
-        })
-    }
+  if (result) {
+    return res.status(200).json({
+      message: "Todo Created Successfully"
+    })
+  }
 
-    return res.status(411).json("Error in Todo creation");
+  return res.status(411).json("Error in Todo creation");
 });
 
 
-todoRouter.put('/done', authMiddleware , async(req :CustomRequest, res :Response) => {
-    const todoId :string = req.query.id as string;
+todoRouter.put('/done', authMiddleware, async (req: CustomRequest, res: Response) => {
+  const todoId: string = req.query.id as string;
 
-    const result = await markDoneTodo(Number(todoId));
+  const result = await markDoneTodo(Number(todoId));
 
-    if(result){
-        return res.status(200).json("Todo completed");
-    }
+  if (result) {
+    return res.status(200).json("Todo completed");
+  }
 
-    return res.status(411).json("Error in Mark as Done, Pls try again!");
+  return res.status(411).json("Error in Mark as Done, Pls try again!");
 });
 
-todoRouter.get('/todos', authMiddleware , async(req :CustomRequest, res :Response) => {
-    const userId = req.userId
+todoRouter.get('/todos', authMiddleware, async (req: CustomRequest, res: Response) => {
+  const userId = req.userId
 
-    const result = await getTodo(Number(userId));
+  const result = await getTodo(Number(userId));
 
-    if(result){
-        return res.status(200).json({
-            todos : result
-        });
-    }
+  if (result) {
+    return res.status(200).json({
+      todos: result
+    });
+  }
 
-    return res.status(411).json("Can't get all the todos, Pls try again!");
+  return res.status(411).json("Can't get all the todos, Pls try again!");
 });
 
-todoRouter.delete('/delete', authMiddleware , async(req :CustomRequest, res :Response) => {
-    const todoId :string = req.query.id as string;
+todoRouter.delete('/delete', authMiddleware, async (req: CustomRequest, res: Response) => {
+  const todoId: string = req.query.id as string;
 
-    const result = await deleteTodo(Number(todoId));
+  const result = await deleteTodo(Number(todoId));
 
-    if(result){
-        return res.status(200).json("Todo Deleted");
-    }
+  if (result) {
+    return res.status(200).json("Todo Deleted");
+  }
 
-    return res.status(411).json("Error in Deleting, Pls try again!");
+  return res.status(411).json("Error in Deleting, Pls try again!");
 });
 
 export default todoRouter;
